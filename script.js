d999eedff6ea09e7fd5a58ec67b1af59836a8e2f
Akshat Singh <akshatofficial041@gmail.com>
2024-09-10 14:39:14 +0530

    created a function to generate random choice from 'rock', 'paper', and 'scissor'

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// generates a random choice from 'rock', 'paper', and 'scissor'
+function getComputerChoice() {
+  // generates a random integer from 0 to less than 3
+  const randomNum = Math.trunc(Math.random() * 3);
 
+  let compChoice = "";
+
+  // returns random choice from 'rock', 'paper', and 'scissor'
+  if (randomNum == 0) compChoice = "rock";
+  else if (randomNum == 1) compChoice = "paper";
+  else compChoice = "scissor";
+
+  return compChoice;
+}
